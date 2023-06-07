3999ad3347c75eca8b9e47e3cfb98baf06ce5d46
David Ding <dingtechxing16@gmail.com>
2023-06-07 23:01:04 +0100

    Added city and country props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./styles/index.css"
-import App from "./components/App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./components/App";
+import forecast from "./data/forecast.json";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App location={forecast.location} />
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
