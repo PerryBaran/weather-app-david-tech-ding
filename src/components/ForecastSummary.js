df5a0eefef565f121622eccd36b3f7af632c54de
David Ding <dingtechxing16@gmail.com>
2023-06-12 15:49:19 +0100

    Snapshot testing for ForecastSummary

---
+++ src/components/ForecastSummary.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ForecastSummary(props) {
+  const { date, description, icon, temperature } = props;
+  return (
+    <div className="forecast-summary">
+      <div className="forecast-summary_date">{date}</div>
+      <div className="forecast-summary_icon">{icon}</div>
+      <div className="forecast-summary_temperature">
+        {temperature.max}&deg;C
+      </div>
+      <div className="forecast-summary_description">{description}</div>
+    </div>
+  );
+}
+
+export default ForecastSummary;
