b59c019e1fd1cb62454633d19f4846d8027b9242
David Ding <dingtechxing16@gmail.com>
2023-06-12 16:18:07 +0100

    Added value testing for ForecastSummary

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -5,8 +5,8 @@ import ForecastSummary from "../../components/ForecastSummary";
 describe("ForecastSummary", () => {
   const validProps = {
     date: 1111111,
-    description: "Stub",
-    icon: "StubIcon",
+    description: "Stub description",
+    icon: "stubIcon",
     temperature: {
       min: 12,
       max: 22,
@@ -21,7 +21,36 @@ describe("ForecastSummary", () => {
         temperature={validProps.temperature}
       />
     );
-
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders correct values for props", () => {
+    const { getByText } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(getByText("1111111")).toHaveAttribute(
+      "class",
+      "forecast-summary_date"
+    );
+    expect(getByText("Stub description")).toHaveAttribute(
+      "class",
+      "forecast-summary_description"
+    );
+
+    expect(getByText("stubIcon")).toHaveAttribute(
+      "class",
+      "forecast-summary_icon"
+    );
+
+    expect(getByText("22°C")).toHaveAttribute(
+      "class",
+      "forecast-summary_temperature"
+    );
   });
 });
